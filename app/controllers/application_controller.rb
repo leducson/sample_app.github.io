81eabb20f0a3dc690dc75fc19b03b351a3d54349
Son Le Duc <ducson.aptech@gmail.com>
2018-08-06 15:35:21 +0700

    chapter 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_locale
+  before_action :load_locale
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found?
 
   def hello
     render html: "hello, world!"
   end
 
-  def set_locale
+  def load_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def not_found?
+    render file: "#{Rails.root}/public/404.html", status: 403, layout: false
   end
 end
